0eb7e09f840ba71129a99647127a44034cca66bb
abdul <abdul.isegen@gmail.com>
2023-09-10 10:43:30 -0500

    Created the Item class which would house the data from the json. which would be what is held by the ItemAdapter

--- app/src/main/java/Interview/fetchApp/ItemAdapter.java
+++ app/src/main/java/Interview/fetchApp/ItemAdapter.java
@@ -1,4 +1,67 @@
 package Interview.fetchApp;
 
-public class ItemAdapter {
+import android.content.ClipData;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Item> itemList;
+
+    public ItemAdapter(Context context, List<Item> itemList) {
+        this.context = context;
+        this.itemList = itemList;
+        filterAndSortData();
+    }
+    
+    @NonNull
+    @Override
+    public ItemAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+
+        ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+    
+    private void filterAndSortData() {
+        // Filter out items with blank or null names
+        itemList.removeIf(item -> item.getName() == null || item.getName().trim().isEmpty());
+
+        // Sort the items by listId and then by name
+        Collections.sort(itemList, new Comparator<Item>() {
+            @Override
+            public int compare(Item item1, Item item2) {
+                int listIdComparison = Integer.compare(item1.getListId(), item2.getListId());
+                if (listIdComparison != 0) {
+                    return listIdComparison;
+                }
+                return item1.getName().compareTo(item2.getName());
+            }
+        });
+    }
 }
